aaf857f1f08133ebe4d85d098cd1ea5eea5b8fe9
guilemon <kkanchika@gmail.com>
2017-11-17 20:01:00 +0530

    added full code

--- skating-bear.js
+++ skating-bear.js
@@ -1,11 +1,15 @@
 enchant();
 window.onload = function(){
-    game = new Game();
-    game.onload = function(){ //prepares the game
-        hello = new Label("Hello Bear"); //Creates new label
-        hello.x = 20;
-        hello.y = 160;
-        game.rootScene.addChild(hello);
+    var game = new Core(450,450);//(gameWindow.width,gameWindow.height)
+    game.preload('chara1.png');
+    game.onload = function(){
+        var bear = new Sprite(32,32);
+        bear.image = game.assets['chara1.png'];
+        bear.frame = 4;
+        bear.addEventListener(Event.ENTER_FRAME, function(){
+            this.x+=3;
+        })
+        game.rootScene.addChild(bear);
     }
     game.start();
 }
